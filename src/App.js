1010043531dadb34bed1176df783b9f236c9a115
unknown <hrithikkoduri@gmail.com>
2023-06-11 20:46:31 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,36 +1,75 @@
 import { Alchemy, Network } from 'alchemy-sdk';
-import { useEffect, useState } from 'react';
-
+import React, { Component } from 'react';
+import Layout from './components/Layout';
+import { Card } from 'semantic-ui-react';
+ 
 import './App.css';
 
-// Refer to the README doc for more information about using API
-// keys in client-side code. You should never do this in production
-// level code.
 const settings = {
   apiKey: process.env.REACT_APP_ALCHEMY_API_KEY,
   network: Network.ETH_MAINNET,
 };
 
-
-// In this week's lessons we used ethers.js. Here we are using the
-// Alchemy SDK is an umbrella library with several different packages.
-//
-// You can read more about the packages here:
-//   https://docs.alchemy.com/reference/alchemy-sdk-api-surface-overview#api-surface
 const alchemy = new Alchemy(settings);
 
-function App() {
-  const [blockNumber, setBlockNumber] = useState();
+class App extends Component {
+  state = {
+    blockNumber: '',
+    transactions: [],
+    last5Blocks: [],
+  };
 
-  useEffect(() => {
-    async function getBlockNumber() {
-      setBlockNumber(await alchemy.core.getBlockNumber());
+  async componentDidMount() {
+    const blockNumber = await alchemy.core.getBlockNumber();
+    const { transactions } = await alchemy.core.getBlockWithTransactions(blockNumber);
+    const last5Blocks = await this.fetchLast5Blocks(blockNumber);
+    this.setState({ blockNumber, transactions, last5Blocks });
+  }
+
+  fetchLast5Blocks = async (latestBlockNumber) => {
+    const blocks = [];
+
+    for (let i = latestBlockNumber; i > latestBlockNumber - 5; i--) {
+      const block = await alchemy.core.getBlock(i);
+      console.log(block.number);
+      blocks.push(block.number);
     }
 
-    getBlockNumber();
-  });
+    const items = blocks.map((block) => {
+      return {
+        header : 'Block Number :',
+        description :  <p key={block}> {block}</p>,
+        style: { overflowWrap: 'break-word '},
+        fluid: true
+      }
+    });
 
-  return <div className="App">Block Number: {blockNumber}</div>;
+    return <Card.Group items={items}/>
+  };
+
+  render() {
+    const { blockNumber, transactions, last5Blocks } = this.state;
+    if (!blockNumber || !transactions || !last5Blocks) {
+      return <div>Loading...</div>;
+    }
+    return (
+      <Layout>      
+        <div className="App">
+          <h1>Last 5 Blocknumbers:</h1>
+          {last5Blocks}
+         <br></br>
+          <h2>Latest Block Number: {blockNumber}</h2>
+          <hr></hr>
+          <h2>Transactions on {blockNumber}:</h2>
+          <ol>
+            {transactions.map((transaction) => (
+              <li key={transaction.hash}>{transaction.hash}</li>
+            ))}
+          </ol> 
+      </div>
+      </Layout>
+    );
+  }
 }
 
 export default App;
